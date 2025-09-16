0e8d3b62fe19c1d0ed5619643ef40446b6650b98
Raymond Pang <124433610+pppkkkkkkk@users.noreply.github.com>
2025-09-16 23:26:35 +0000

    Tuned Version

--- code.py
+++ code.py
@@ -1,13 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        dict = {}
         for i in range(len(nums)):
-            for j in range(j,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        return [-1, -1];
+            diff = target - nums[i]
+            if nums[i] in dict:
+                return [dict[nums[i]],i]
+            dict[diff] = i
         
+            
